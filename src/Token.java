ee914cf112fd084dd2fa43c312c1c265bb117950
Tanner Smith <tanner@tssoftware.net>
2012-04-26 17:15:17 -0400

    Document Token.

--- src/Token.java
+++ src/Token.java
@@ -1,13 +1,24 @@
-
+/**
+ * Token with a name and a TokenType.
+ */
 public class Token {
 	private String string;
 	private TokenType type;
 	
+	/**
+	 * Construct a new Token with a String.
+	 * @param string String
+	 */
 	public Token(String string) {
 		this.string = string;
 		this.type = identifyType(string);
 	}
 	
+	/**
+	 * Identify the TokenType from the String value.
+	 * @param string String to determine TokenType from
+	 * @return Resulting TokenType
+	 */
 	private TokenType identifyType(String string) {
 		for (TokenType type : TokenType.values()) {
 			if (string.matches(type.getRegex())) {
@@ -18,11 +29,16 @@ public class Token {
 		return TokenType.T_UNKNOWN;
 	}
 	
-	public String toString() {
-		return string;
-	}
-	
+	/**
+	 * Return the TokenType of the Token.
+	 * @return TokenType
+	 */
 	public TokenType getType() {
 		return type;
+	}
+	
+	@Override
+	public String toString() {
+		return string;
 	}
 }
