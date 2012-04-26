2e4f33f3cb0e927210d51034315be7907412599f
Tanner Smith <tanner@tssoftware.net>
2012-04-26 17:05:36 -0400

    Document Variable.

--- src/Variable.java
+++ src/Variable.java
@@ -1,8 +1,12 @@
-
+/**
+ * Variable represented by a String.
+ */
 public class Variable extends RuleElement {
-
+	/**
+	 * Construct a new Variable with a String identifier.
+	 * @param identifier String identifier
+	 */
 	public Variable(String identifier) {
 		super(identifier);
 	}
-
 }
