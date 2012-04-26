1fc51313e4a68687e631d5b5ac0983474415377b
Tanner Smith <tanner@tssoftware.net>
2012-04-26 17:09:12 -0400

    Document Terminal.

--- src/Terminal.java
+++ src/Terminal.java
@@ -1,10 +1,19 @@
-
+/**
+ * Terminal that has a String identifier.
+ */
 public class Terminal extends RuleElement {
-
+	/**
+	 * Construct a new Terminal with a String identifier.
+	 * @param identifier String identifier
+	 */
 	public Terminal(String identifier) {
 		super(identifier);
-	}	
+	}
 	
+	/**
+	 * Return the TokenType from the Identifier for this Terminal.
+	 * @return TokenType
+	 */
 	public TokenType tokenType() {
 		for (TokenType tt : TokenType.values()) {
 			if (tt.getIdentifier().equals(getIdentifier())) {
