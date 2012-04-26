f217ea2f8a1c3bb84fd7546c7f679460886944bf
Tanner Smith <tanner@tssoftware.net>
2012-04-26 17:07:12 -0400

    Document RuleElement.

--- src/RuleElement.java
+++ src/RuleElement.java
@@ -1,19 +1,31 @@
-
+/**
+ * RuleElement that has a String identifier.
+ */
 public class RuleElement {
 	private String identifier;
 	
+	/**
+	 * Construct a new RuleElement with a String identifier.
+	 * @param identifier String identifier
+	 */
 	public RuleElement(String identifier) {
 		this.identifier = identifier;
 	}
 	
+	/**
+	 * Return the identifier of the RuleElement.
+	 * @return Identifier of the RuleElement
+	 */
 	public String getIdentifier() {
 		return identifier;
 	}
 	
+	@Override
 	public String toString() {
 		return identifier;
 	}
 	
+	@Override
 	public boolean equals(Object o) {
 		if (o instanceof RuleElement) {
 			RuleElement other = (RuleElement)o;
