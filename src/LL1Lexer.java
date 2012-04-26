892b2d95afb29a1b87c9a1a7aecfb03ac7edba22
Tanner Smith <tanner@tssoftware.net>
2012-04-26 17:54:28 -0400

    Document LL1Lexer.

--- src/LL1Lexer.java
+++ src/LL1Lexer.java
@@ -4,11 +4,17 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-
+/**
+ * LL1 Lexer that reads a file and generates Tokens.
+ */
 public class LL1Lexer {
 	private Scanner scanner;
 	private Queue<Token> tokenBuffer;
 	
+	/**
+	 * Construct a new LL1 LExer with the given file.
+	 * @param f File to perform lexical analysis on.
+	 */
 	public LL1Lexer(File f) {
 		try {
 			scanner = new Scanner(f);
@@ -19,14 +25,25 @@ public class LL1Lexer {
 		}
 	}
 	
+	/**
+	 * Determine if the lexer has anything left to parse.
+	 * @return Return true when the Scanner has items left or the tokenBuffer is not empty
+	 */
 	public boolean hasNext() {
 		return scanner.hasNext() || !tokenBuffer.isEmpty();
 	}
 	
+	/**
+	 * Add a Token to the tokenBuffer based off the given String.
+	 * @param s String to tokenize and add to the tokenBuffer
+	 */
 	public void fillTokenBuffer(String s) {
 		StringBuilder sb = new StringBuilder();
+		
 		for (int i = 0; i < s.length(); i++) {
+			// Loop through all characters in the string
 			char c = s.charAt(i);
+			
 			if (c == ' ' || c == ',' || c == ';' || c == '&' || c == '+' || c == '-' || c == '(' || c == ')' || c == '%' || c == '*') {
 				if (sb.toString().length() > 0) {
 					tokenBuffer.add(new Token(sb.toString()));
@@ -50,6 +67,10 @@ public class LL1Lexer {
 		}
 	}
 	
+	/**
+	 * Step through the Scanner until the tokenBuffer contains something.
+	 * @return Return the first Token inserted in the tokenBuffer
+	 */
 	public Token next() {
 		if (!scanner.hasNext() && tokenBuffer.isEmpty()) {
 			return null;
