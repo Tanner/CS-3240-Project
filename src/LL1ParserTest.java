592f5d813e199be635f583b5a15e130d590a9f42
Tanner Smith <tanner@tssoftware.net>
2012-04-26 18:03:07 -0400

    Document LL1ParserTest.

--- src/LL1ParserTest.java
+++ src/LL1ParserTest.java
@@ -4,6 +4,9 @@ import java.io.File;
 
 import org.junit.Test;
 
+/**
+ * LL1 Parser JUnit Tests
+ */
 public class LL1ParserTest {
 	@Test
 	public void testLL1ParserTest1() throws LL1ParseException {
@@ -25,6 +28,12 @@ public class LL1ParserTest {
 		testGrammar("tests/tiny_precedence/grammar.txt", "tests/tiny_precedence/test.txt");
 	}
 
+	/**
+	 * Quickly create an LL1 Parser with the given file paths and assert the result of the parse.
+	 * @param grammarPath Path to the grammar file
+	 * @param pathToParse Path to the file to parse
+	 * @throws LL1ParseException
+	 */
 	public void testGrammar(String grammarPath, String pathToParse) throws LL1ParseException {
 		LL1Parser parser = new LL1Parser(new File(grammarPath), new File(pathToParse));
 		assertTrue(parser.parse());
