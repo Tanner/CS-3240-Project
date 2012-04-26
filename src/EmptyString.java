45f195e975836cdf7ce351c84dd109bc6384dc00
Tanner Smith <tanner@tssoftware.net>
2012-04-26 17:21:22 -0400

    Document EmptyString.

--- src/EmptyString.java
+++ src/EmptyString.java
@@ -1,8 +1,11 @@
-
+/**
+ * Terminal that represents the EmptyString.
+ */
 public class EmptyString extends Terminal {
-
+	/**
+	 * Construct a new EmptyString represented by the identifer of "{epsilon}".
+	 */
 	public EmptyString() {
 		super("{epsilon}");
 	}
-	
 }
