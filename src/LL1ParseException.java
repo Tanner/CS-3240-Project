059b9ad8a7c35ee4dc291f021371f29fb807408a
Tanner Smith <tanner@tssoftware.net>
2012-04-26 17:55:18 -0400

    Document LL1ParseException.

--- src/LL1ParseException.java
+++ src/LL1ParseException.java
@@ -1,8 +1,12 @@
-
+/**
+ * Custom Exception for LL1 Parsing use only.
+ */
 public class LL1ParseException extends Exception {
-
+	/**
+	 * Construct a new LL1 Parse Exception with the given message.
+	 * @param string Message
+	 */
 	public LL1ParseException(String string) {
 		super(string);
 	}
-
 }
